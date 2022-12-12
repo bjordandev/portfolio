826743b5b5a67dd447db24f30bcde9e84a8c940c
bjordandev <b.jordan.dev@outlook.com>
2022-12-12 16:21:39 +0100

    create HeadingM component

---
+++ src/components/typography/HeadingM/index.tsx
@@ -0,0 +1,24 @@
+import React, { ElementType, HTMLAttributes, FC} from "react"
+import * as styles from "./heading-m.module.css";
+
+import { getCSSVariable } from "../../../helpers";
+
+interface TypographyProps extends HTMLAttributes<HTMLOrSVGElement> {
+  as?: ElementType;
+  children?: string;
+  color?: string;
+}
+
+const HeadingM: React.FC<TypographyProps> = ({
+  as: Tag = 'h2',
+  children,
+  color = getCSSVariable("--theme-color-text")
+}) => {
+  return (
+    <Tag className={styles.headingM} style={{color}}>
+      { children }
+    </Tag>
+  );
+}
+
+export default HeadingM;
