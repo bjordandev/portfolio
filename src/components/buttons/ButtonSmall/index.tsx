6b7d20ddde6171bd363b73a407bb0a0adc2ca88e
bjordandev <b.jordan.dev@outlook.com>
2022-12-12 17:45:38 +0100

    create button small component

---
+++ src/components/buttons/ButtonSmall/index.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from "react"
+import * as styles from "./button-small.module.css";
+
+type ButtonSmallProps = {
+  children?: string;
+  handleClick: (event: React.MouseEvent<HTMLElement>) => true;
+}
+
+const ButtonSmall: FC<ButtonSmallProps> = ({
+  children,
+  handleClick
+}) => {
+  return (
+    <button className={styles.buttonSmall} onClick={handleClick}>
+      { children }
+    </button>
+  );
+}
+
+export default ButtonSmall;
