c9bbc54373579178d39f06b62435d8499fc1ddd2
bjordandev <b.jordan.dev@outlook.com>
2022-12-23 15:22:18 +0100

    add fontawesome library

--- src/components/templates/Page.tsx
+++ src/components/templates/Page.tsx
@@ -1,34 +1,41 @@
-import * as React from "react"
+import * as React from "react";
 import "../../styles/reset.css";
-import "../../styles/index.css"; 
+import "../../styles/index.css";
 
 import { Helmet } from "react-helmet";
 
-type ChildrenProps = { 
+type ChildrenProps = {
   children: React.ReactNode
 }
 
 const LayoutHeader: React.FC<any> = () => {
   return (
     <Helmet>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;800;900&family=Source+Code+Pro:ital,wght@0,900;1,900&display=swap" rel="stylesheet" />
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+      <link
+        rel="prefetch"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+      />
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;800;900&family=Source+Code+Pro:ital,wght@0,900;1,900&display=swap"
+        rel="stylesheet"
+      />
     </Helmet>
   )
 }
 
-const Layout: React.FC<ChildrenProps> = ({
-  children
-}) => {
+const Layout: React.FC<ChildrenProps> = ({ children }) => {
   return (
     <>
       <LayoutHeader />
-      <main>
-        { children }
-      </main>
+      <main>{children}</main>
     </>
   );
 }
