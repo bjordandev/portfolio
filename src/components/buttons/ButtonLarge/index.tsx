7867e7b8ade4a75c8e68bc8dcbacf1d235130ff0
bjordandev <b.jordan.dev@outlook.com>
2022-12-23 09:21:55 +0100

    create buttonLarge component

---
+++ src/components/buttons/ButtonLarge/index.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from "react"
+import * as styles from "./button-large.module.css";
+
+type ButtonLargeProps = {
+  children?: string;
+  handleClick?: (event: React.MouseEvent<HTMLElement>) => true;
+}
+
+const ButtonLarge: FC<ButtonLargeProps> = ({
+  children,
+  handleClick
+}) => {
+  return (
+    <button className={styles.buttonLarge} onClick={handleClick}>
+      { children }
+    </button>
+  );
+}
+
+export default ButtonLarge;
