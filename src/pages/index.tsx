8848be9ffba0cd801e7ae4fbb1469aff1174e639
bjordandev <b.jordan.dev@outlook.com>
2022-12-23 16:43:24 +0100

    fix hover style on large button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,24 @@ import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
 import Layout from "../components/templates/Page"
 
+import ButtonMedium from "../components/buttons/ButtonMedium"
+import ButtonSmall from "../components/buttons/ButtonSmall"
+import ButtonLarge from "../components/buttons/ButtonLarge"
+import TextInput from "../components/TextInput"
+import SelectInput from "../components/SelectInput"
+
+
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <Layout>
-      It works
+      <ButtonSmall>Hello world</ButtonSmall>
+      <ButtonMedium>Hello World</ButtonMedium>
+      <ButtonLarge>Hello World</ButtonLarge>
+      <div style={{width:'300px'}}>
+        <TextInput valid={true} value="toto"/>
+        <SelectInput options={['a', 'b', 'c']} />
+      </div>
+      
     </Layout>
   )
 }
